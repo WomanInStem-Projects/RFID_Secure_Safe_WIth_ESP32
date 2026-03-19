d1da4f502184686959d17f8d5dc9c86c12ddd7c4
kelly202 <jennifertanekeu18@gmail.com>
2026-03-19 10:50:58 -0400

    Code updated. rfid works on wowki. Flashing to esp32 not working. Will solder pins to rfid module

--- Firware/src/main.cpp
+++ Firware/src/main.cpp
@@ -3,14 +3,25 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-// ANSWERS TO YOUR QUESTIONS:
-// #define vs const? 
-// - #define: preprocessor directive, replaced at compile time, no type checking
-// - const: actual variable with type checking, takes memory, safer
-// 
-// What defines baud rate?
-// - Serial.begin(115200) in setup() defines the baud rate
-// - Must match monitor_speed in platformio.ini
+// function declarations
+
+// display functions
+void blinkled();
+void clearDisplay();
+void waitForSerial(unsigned long timeout = 10000);
+
+
+// rfid tag/fob functions
+bool cardInacl();
+void lookinforcard();
+
+// access control list functions
+void addCardtoACL();
+void AccessControl();
+boolean IsAdmin();
+void AccessDenied();
+void AccessGranted();
+
 
 const int redled = 26;
 const int greenled = 21;
@@ -20,17 +31,19 @@ String guessed = "";
 String cardID = "";
 bool pswdset = false;
 
+// oled pins
 const int sda = 13;
 const int scl = 14;
 
 const int sck = 18;   // ESP32 default SPI clock
 const int miso = 19;  // ESP32 default SPI MISO
-const int mosi = 23;  // ESP32 default SPI MOSI
+const int mosi = 23;  // ESP32 default SPI MOSI is 23, 18
 const int ss = 5;     // ESP32 default SPI CS
 const int rst = 4;   // RFID reset pin
 bool cardpresent = false;
 
-String ACL[]; // access controll list contsining each UI for RFID tag/card
+String ACL[10]; // access controll list contsining each UI for RFID tag/card
+int aclSize = 0;
 
 MFRC522 rfid(ss, rst);
 Adafruit_SSD1306 oled(128, 64, &Wire, -1);
@@ -40,82 +53,124 @@ void clearDisplay() {
   oled.setCursor(0, 0);  // Reset cursor to top-left
 }
 
-void addCardtoACL(String cardID) {
-  //ACL[sizeof(ACL) / sizeof(ACL[0])] = cardID; // Add card ID to ACL
+void addCardtoACL() {
+  //check i card isnt in acl
+  if (cardInacl() == false){
+  // if its not add it to the acl
+  ACL[aclSize] = cardID;
+  aclSize++;
+  }
+
+  
+}
+
+bool cardInacl()
+{
+  for (int i = 0; i < aclSize; i++) 
+  {
+      if (ACL[i] == cardID) 
+      {
+        displayAndSerial("Card already in ACL");
+        return true;
+      }
+
+      
+  }
+  return false;
 }
 
 void lookinforcard() //loops untill a card is found, if card is found prints to serial
 {
   while (true){
-    if ( rfid.PICC_IsNewCardPresent()) // loop ag
+    if ( rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) // loop ag
     {
-      break; //card is close by so break out of loop
-
-    } 
-    displayAndSerial("No Card Detected - Please Tap Your Card");
-    delay(1000);
-  
-
-    if ( ! rfid.PICC_ReadCardSerial())  // if there is a tag present but cant read it, loop again
-    {
-      displayAndSerial("Card Exists but Cannot get UID");
-    } else { // get card UID
-      for (byte i = 0; i < rfid.uid.size; i++) {
+      for (byte i = 0; i < rfid.uid.size; i++) 
+      {
         cardID += String(rfid.uid.uidByte[i], HEX);
       }
       cardID.toUpperCase();
       displayAndSerial("Card Detected with UID: " + cardID);
-
-      Serial.println();
-
-      rfid.PICC_HaltA(); // this closed the reader's connection to the card, so we can read another card after this
-      //rfid.PCD_StopCrypto1(); // stop encryption on PCD, i dont think its encrypted fr
+      break; // out of while loop
     }
+    
+    Serial.println();
+    displayAndSerial("No Card Detected - Please Tap Your Card");
+    //delay(1000);
   }
+
+  rfid.PICC_HaltA(); // this closed the reader's connection to the card, so we can read another card after this
+  delay(1000);  
 
 }
 
+void AccessDenied()
+{
+  blinkled(redled);
+  displayAndSerial("Access Denied");
+  //digitalWrite(redled, HIGH);
+  //delay(2000);
+}
+
+void AccessGranted()
+{
+  blinkled(greenled);
+  displayAndSerial("Access Granted");
+  //digitalWrite(greenled, HIGH);
+  //delay(2000);
+  //digitalWrite(greenled, LOW);
+}
 void AccessControl() {
   lookinforcard();
   // if card is in ACL, grant access, else deny access
-  displayAndSerial("Would you like to add this card to the Access Control List? (y/n)");
-  while (Serial.available() == 0) {}
-  waitForSerial();
-  String response = Serial.readStringUntil('\n');
-  response.trim();
 
-  // checks user response
-  // if yes prompts for admin password
-  // if no returns to main loop and waits for another card!!
-  switch (response.c_str()[0]) { 
-    // User wants to add card to ACL
-    case 'y':
-    case 'Y':
-      if (IsAdmin()) {
-        displayAndSerial("Adding Card to ACL");
-        delay(1000);
-        addCardtoACL(cardID);
-        delay(1000);
-        displayAndSerial("Card Added to ACL");
-      break;
-      } else {
-        displayAndSerial("Admin Access Denied - Cannot Add Card to ACL");
-        return;
-      }
-    // User doesnt want to add card to ACL
-    case 'n':
-    case 'N':
-      displayAndSerial("Card Not Added to ACL");
-      break;
-    default:
-      displayAndSerial("Invalid Response - Card Not Added to ACL");
+  if (cardInacl() == true) 
+  {
+    AccessGranted();
+  } else {
+    AccessDenied();
+
+    displayAndSerial("Would you like to add this card to the Access Control List? (y/n)");
+    while (Serial.available() == 0) {}
+    waitForSerial();
+    String response = Serial.readStringUntil('\n');
+    response.trim();
+
+    // checks user response
+    // if yes prompts for admin password
+    // if no returns to main loop and waits for another card!!
+    switch (response.c_str()[0]) { 
+      // User wants to add card to ACL
+      case 'y':
+      case 'Y':
+        if (IsAdmin()) {
+          displayAndSerial("Adding Card to ACL");
+          //delay(1000);
+          addCardtoACL();
+          //delay(1000);
+          displayAndSerial("Card Added to ACL");
+        break;
+        } else {
+          displayAndSerial("Admin Access Denied - Cannot Add Card to ACL");
+          return;
+        }
+      // User doesnt want to add card to ACL
+      case 'n':
+      case 'N':
+        displayAndSerial("Card Not Added to ACL");
+        break;
+      default:
+        displayAndSerial("Invalid Response ");
   }
+
+  }
+
+
+
 }
 
 // ask usrer for admin password to allow them to set UID
 // always returns true for now because they have unlimited attempts (attack vector) , fix later
-boolean IsAdmin() 
-{
+boolean IsAdmin() {
   boolean isAdmin;
 
   displayAndSerial("Enter Admin password:");
@@ -124,35 +179,60 @@ boolean IsAdmin()
   guessed = Serial.readStringUntil('\n');
   guessed.trim();
   displayAndSerial("You Entered: " + guessed);
-  delay(2000); //give em time to read display
+  //delay(2000); //give em time to read display
 
-  while (guessed != pswd) {
+  while (guessed != pswd){ 
     isAdmin = false;
-    blinkled(redled);
-    displayAndSerial("Access Denied - Try Again");
-    delay(1000);
-    
-    displayAndSerial("Enter Admin password:");
-    
+    AccessDenied();
+    //delay(1000);
+
+    displayAndSerial("Try again? (y/n)");
     while (Serial.available() == 0) {}
-    delay(500);
+    waitForSerial();
+    //delay(2000);
+    String answer = Serial.readStringUntil('\n');
+    answer.trim();
+
+    switch(answer.c_str()[0]) 
+    {
+      case 'y':
+      case 'Y':
     
-    guessed = Serial.readStringUntil('\n');
-    guessed.trim();
-    displayAndSerial("You Entered: " + guessed);
+        displayAndSerial("Enter Admin password:");
+        
+        while (Serial.available() == 0) {}
+        waitForSerial();
+        //delay(500);
+        
+        guessed = Serial.readStringUntil('\n');
+        guessed.trim();
+        displayAndSerial("You Entered: " + guessed);      
+        break; // continue while loop 
+
+      case 'n':
+      case 'N':
+        return isAdmin;
+      
+      default:
+        displayAndSerial("Invalid Response ");
+
+    }
+
+
 
   }
-  
+
+
+  AccessGranted();
   displayAndSerial("Access Granted. ");
+  // Will add code for buzzer sound here later.
   isAdmin = true;
-  delay(1000);
-  digitalWrite(redled, LOW);
-  blinkled(greenled);
-  digitalWrite(greenled, HIGH);
   
   guessed = "";  // Reset for next round
-  delay(3000);
-  digitalWrite(greenled, LOW);
+  //delay(3000);
+
+  return isAdmin;
+
 }
 
 void blinkled(int pin) {
@@ -177,9 +257,9 @@ void displayAndSerial(String message) {
   oled.setTextColor(SSD1306_WHITE);
   oled.println(message);
   oled.display();  //render to screen
-  delay(500);
+  delay(2000);
 }
-void waitForSerial(unsigned long timeout = 10000) {
+void waitForSerial(unsigned long timeout) {
   unsigned long start = millis();
   while (Serial.available() == 0 && millis() - start < timeout) {}
   delay(500);
@@ -200,6 +280,9 @@ void setup() {
   // Initialize SPI for RFID, which will be added later
   SPI.begin(sck, miso, mosi, ss);
   rfid.PCD_Init();
+  byte version = rfid.PCD_ReadRegister(MFRC522::VersionReg);
+  Serial.print("MFRC522 version: 0x");
+  Serial.println(version, HEX);
   displayAndSerial("Starting....");
   
   
@@ -210,7 +293,7 @@ void setup() {
   // LED PINMODES
   pinMode(redled, OUTPUT);
   pinMode(greenled, OUTPUT);
-  digitalWrite(redled, HIGH);
+  //digitalWrite(redled, HIGH);
 }
 
 void loop() {
@@ -229,7 +312,7 @@ void loop() {
     
     if (pswdlength < 6){ //if password aint 6 chars restart the entire loop
        displayAndSerial("Password must be 6 characters");
-       delay(2000);
+       //delay(2000);
        return ;
       }
     displayAndSerial("You Entered: ******");
